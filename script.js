cb53760a065509d0b92bf169372689857e1bee98
Sonu Kumar <43966361+sonukumar0109@users.noreply.github.com>
2026-04-17 11:56:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
-// Loader remove
-window.addEventListener("load", () => {
-  document.querySelector(".loader").style.display = "none";
-});
+const name = "Hello Motu ❤️"; // 👉 yaha uska naam likh
 
-// Scroll function
-function scrollToSection() {
-  document.querySelector(".about").scrollIntoView({
-    behavior: "smooth"
-  });
-}
+const text = `Hello ${Motu}... 💖 Do You still Love me the same ?`;
 
-// Typing effect
-const text = "Hello, I am Developer";
 let i = 0;
 
 function typing() {
   if (i < text.length) {
     document.getElementById("text").innerHTML += text.charAt(i);
     i++;
-    setTimeout(typing, 100);
+    setTimeout(typing, 70);
   }
 }
 
-document.getElementById("text").innerHTML = "";
 typing();
+
+// YES click
+function yesClick() {
+  document.getElementById("result").innerHTML =
+    "Yayyyy 💖 I know you love me so much 😭❤️ so do I ";
+  document.body.style.background = "#ff4d6d";
+}
+
+// NO button bhaagega 😂
+const noBtn = document.getElementById("noBtn");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * window.innerWidth - 100;
+  const y = Math.random() * window.innerHeight - 50;
+
+  noBtn.style.position = "absolute";
+  noBtn.style.left = x + "px";
+  noBtn.style.top = y + "px";
+});
